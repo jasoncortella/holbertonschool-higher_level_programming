2d804fa28401c7d76689ec1949cf82035e7e7035
Jason Cortella <419@holbertonschool.com>
2019-04-30 19:52:34 +0000

    Initial commit p9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        abnumber = -number
+    else:
+        abnumber = number
+    lastdigit = abnumber % 10
+    print("{}".format(lastdigit), end = '')
+    return lastdigit
