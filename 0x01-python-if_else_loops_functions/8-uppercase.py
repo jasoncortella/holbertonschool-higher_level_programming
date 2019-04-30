222bddd2edde71d3a997cfa1c36bf5d651f5326d
Jason Cortella <419@holbertonschool.com>
2019-04-30 19:46:24 +0000

    Initial commit p8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        x = c;
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            x = ord(c) - 32
+        else:
+            x = ord(c)
+        print("{:c}".format(x), end='')
+    print("")
