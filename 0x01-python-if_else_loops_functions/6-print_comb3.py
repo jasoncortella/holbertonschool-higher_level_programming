57583994bc6379fc51899413be0068b58c0e40ad
Jason Cortella <419@holbertonschool.com>
2019-04-30 19:15:29 +0000

    Switch print to use format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9 + 1):
     for j in range(i + 1, 9 + 1):
         if i + j < 17:
-            print(i, j, ", ",  sep = '', end = '')
+            print("{}{}, ".format(i, j), end='')
         else:
-            print(i, j, sep = '')
+            print("{}{}".format(i, j))
