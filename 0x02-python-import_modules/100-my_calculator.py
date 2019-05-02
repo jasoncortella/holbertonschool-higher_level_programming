79fd9708fafa6407411484df6318314fb3dfd7f3
Jason Cortella <419@holbertonschool.com>
2019-05-02 19:31:44 +0000

    Clean up import statements

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
-    if len(sys.argv) != 4:
+    from sys import argv
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
     if op == '+':
         result = add(a, b)
     elif op == '-':
