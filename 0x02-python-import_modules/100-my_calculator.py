fc49f6ba6c92b7e35310f0582882555d7521bffd
Jason Cortella <419@holbertonschool.com>
2019-05-02 19:27:17 +0000

    Switch from sys exit to builtin exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
+    from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
