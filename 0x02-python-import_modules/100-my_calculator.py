34a27122503635ee8ea9db74d8ba46ea132a8c7a
Jason Cortella <419@holbertonschool.com>
2019-05-02 19:24:33 +0000

    Restructure if statements

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
     a = int(argv[1])
     op = argv[2]
     b = int(argv[3])
@@ -17,6 +14,9 @@ if __name__ == "__main__":
         result = a - b
     elif op == '*':
         result = a * b
-    else:
+    elif op = '/':
         result = a / b
+    else
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     print("{} {} {} = {}".format(a, op, b, result))
