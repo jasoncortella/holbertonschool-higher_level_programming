b9daeaa721446617335ae4ed6c42ee652806edb7
Jason Cortella <419@holbertonschool.com>
2019-05-07 19:20:29 +0000

    Check that my_list exists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list), 0, -1):
-        print("{:d}".format(i))
+    if my_list:
+        for i in range(len(my_list), 0, -1):
+            print("{:d}".format(i))
