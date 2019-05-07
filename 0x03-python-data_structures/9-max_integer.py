0c047ac1eab310e5e1a081fe03e38d6b39be6472
Jason Cortella <419@holbertonschool.com>
2019-05-07 20:33:47 +0000

    Add coverage for if max is negative

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxret = 0
-    for i in my_list:
-        if i > maxret:
-            maxret = i
+    if my_list:
+        maxret = my_list[0]
+        for i in my_list:
+            if i > maxret:
+                maxret = i
     return maxret if my_list else None
