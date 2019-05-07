eccc83c293febae7a87b8269e35ef5b278411f23
Jason Cortella <419@holbertonschool.com>
2019-05-07 19:22:19 +0000

    Switch from range to ::-1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for i in range(len(my_list), 0, -1):
+        for i in my_list[::-1]:
             print("{:d}".format(i))
