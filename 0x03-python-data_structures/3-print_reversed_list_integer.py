b1474e4b40427d2578cbb295a1e17ffb5448e539
Jason Cortella <419@holbertonschool.com>
2019-05-07 19:11:44 +0000

    Switch {} to {:d}

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list), 0, -1):
+        print("{:d}".format(i))
