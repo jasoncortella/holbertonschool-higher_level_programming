803899be5977569b7a023b25509f5377a584c499
Jason Cortella <419@holbertonschool.com>
2019-05-07 19:52:48 +0000

    pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
             for j in i:
-                print("{:d}".format(j), end = " " if j%len(i) != 0 else "\n")
+                print("{:d}".format(j), end="\n" if j % len(i) == 0 else " ")
