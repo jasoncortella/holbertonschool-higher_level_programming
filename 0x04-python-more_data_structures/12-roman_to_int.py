07056ee21fabe00e7d0db1cdbc1a7cfa5c60bc6f
Jason Cortella <419@holbertonschool.com>
2019-05-09 20:05:20 +0000

    Initial commit p12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def rconvert(r):
+    if (r == 'I'):
+        return 1
+    if (r == 'V'):
+        return 5
+    if (r == 'X'):
+        return 10
+    if (r == 'L'):
+        return 50
+    if (r == 'C'):
+        return 100
+    if (r == 'D'):
+        return 500
+    if (r == 'M'):
+        return 1000
+
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string == None:
+        return None
+    result = 0
+    rlen = len(roman_string)
+    for i in range(rlen):
+        vcurrent = rconvert(roman_string[i])
+        if (i < rlen - 1):
+            vnext = rconvert(roman_string[i+1])
+            result += vcurrent if vcurrent >= vnext else -vcurrent
+        else:
+            result += vcurrent
+    return result
