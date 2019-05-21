b12ac90f750ec41bdb1b007266a7dcc43bc98142
Jason Cortella <419@holbertonschool.com>
2019-05-21 23:54:19 +0000

    Add posision setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,10 +3,8 @@ class Square:
     """A class to define a square."""
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the class."""
-        if self.__valid_size(size):
-            self.__size = size
-        if self.__valid_position(position):
-            self.__position = position
+            self.size = size
+            self.position = position
 
     @property
     def size(self):
@@ -42,16 +40,14 @@ class Square:
 
     def __valid_position(self, position):
         """Checks if a variable is a tuple of 2 positive integers."""
-        if not isinstance(position, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-            return False
-        if any(not isinstance(num, int) for num in position):
-            raise TypeError("position must be a tuple of 2 positive integers")
-            return False
-        if any(num < 0 for num in position):
-            raise TypeError("position must be a tuple of 2 positive integers")
-            return False
-        return True
+        if isinstance(position, tuple):
+            if len(position) == 2:
+                if isinstance(position[0], int):
+                    if isinstance(position[1], int):
+                        if position[0] >= 0 <= position[1]:
+                            return True
+        raise TypeError("position must be a tuple of 2 positive integers")
+        return False
 
     @size.setter
     def size(self, value):
@@ -59,6 +55,7 @@ class Square:
         if self.__valid_size(value):
             self.__size = value
 
+    @position.setter
     def position(self, value):
         """Sets position."""
         if self.__valid_position(value):
