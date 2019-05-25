413fa0489962b4ab95ff2148e7eed2d0349416ff
Jason Cortella <419@holbertonschool.com>
2019-05-25 16:48:14 +0000

    revert last commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,6 +17,8 @@ def print_square(size):
         - ValueError if size is less than 0
     """
 
+    if size == 0:
+        print()
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
