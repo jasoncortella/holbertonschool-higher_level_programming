9cb798f519867f05b41a4b210b516749ca887462
Jason Cortella <419@holbertonschool.com>
2019-07-16 21:36:38 +0000

    Add primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@
    -- If the table states already exists, script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-       (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256) NOT NULL);
