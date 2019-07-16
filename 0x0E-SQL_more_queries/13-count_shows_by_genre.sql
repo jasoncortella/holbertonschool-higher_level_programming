5a68628279a586e47f268967b0cd2b0308d2aab5
Jason Cortella <419@holbertonschool.com>
2019-07-16 23:32:59 +0000

    Change JOIN TO INNER JOIN

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@
    -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;
