9eec0dedc26a1237acf2b7564bf01714759d23a0
Jason Cortella <419@holbertonschool.com>
2019-07-16 21:35:21 +0000

    Add primary key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,7 +9,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-       (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        state_id INT NOT NULL,
        FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
        name VARCHAR(256) NOT NULL);
