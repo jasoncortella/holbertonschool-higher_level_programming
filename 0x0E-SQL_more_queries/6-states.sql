a643122f7f7d007c9c12b89dfd8a40fb6ff5262a
Jason Cortella <419@holbertonschool.com>
2019-07-16 20:53:47 +0000

    Initial commit p6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- creates the database hbtn_0d_usa and the table states on your MySQL server.
+   -- states description:
+      -- id INT unique - auto generated, cnt be null, is a primary key
+      -- name VARCHAR(256) - can't be null
+   -- The database name will be passed as an argument of the mysql command
+   -- If the table hbtn_0d_usa already exists, script should not fail
+   -- If the table states already exists, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+       (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256));
