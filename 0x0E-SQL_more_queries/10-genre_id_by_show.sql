8db21ae79e3dacfda1861d39a23543307b9811d0
Jason Cortella <419@holbertonschool.com>
2019-07-16 22:43:57 +0000

    Remove old artifact line

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,5 +6,4 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-     WHERE tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
