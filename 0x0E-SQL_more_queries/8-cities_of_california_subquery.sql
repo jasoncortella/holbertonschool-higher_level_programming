8ab0f526b120ab8e24a1ad366649be1edce6bdf9
Jason Cortella <419@holbertonschool.com>
2019-07-16 21:59:20 +0000

    Initial commit p8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+   -- lists all the cities of California in the database hbtn_0d_usa
+   -- states table contains only one record where name = California
+   -- Results must be sorted in ascending order by cities.id
+   -- Does not use JOIN
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California")
+ORDER BY id;
