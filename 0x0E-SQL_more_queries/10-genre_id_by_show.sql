6236dc67d8263161c71e3f1050911e05491c4651
Jason Cortella <419@holbertonschool.com>
2019-07-16 22:37:15 +0000

    Switch to basic JOIN

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,6 @@
    -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-     WHERE tv_show_genres.show_id IS NOT NULL
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+     WHERE tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@
    -- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON cities.state_id=states.id
+JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
