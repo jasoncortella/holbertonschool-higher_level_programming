a33cf632e287480d802967ad5363c109b9f09385
Jason Cortella <419@holbertonschool.com>
2019-07-16 20:51:46 +0000

    Improve readability

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -5,6 +5,7 @@
    -- The database name will be passed as an argument of the mysql command
    -- If the table unique_id already exists, script should not fail
 
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1,
+CREATE TABLE IF NOT EXISTS unique_id
+       (id INT DEFAULT 1,
        UNIQUE (ID),
        name VARCHAR(256));
