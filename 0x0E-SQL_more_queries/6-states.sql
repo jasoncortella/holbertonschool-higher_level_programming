db86427f26fa1caaef7593f547e23af5794037f8
Jason Cortella <419@holbertonschool.com>
2019-07-16 20:55:39 +0000

    Fix comment typo

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states on your MySQL server.
    -- states description:
-      -- id INT unique - auto generated, cnt be null, is a primary key
+      -- id INT unique - auto generated, cant be null, is a primary key
       -- name VARCHAR(256) - can't be null
    -- The database name will be passed as an argument of the mysql command
    -- If the table hbtn_0d_usa already exists, script should not fail
