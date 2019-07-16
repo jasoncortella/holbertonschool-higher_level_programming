455b54d07ba44722c17e0c4a771cbc3a2eabf955
Jason Cortella <419@holbertonschool.com>
2019-07-16 22:02:13 +0000

    Remove line 0 comment indentation

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,7 @@
-   -- lists all the cities of California in the database hbtn_0d_usa
-   -- states table contains only one record where name = California
-   -- Results must be sorted in ascending order by cities.id
-   -- Does not use J O I N
-   -- The database name will be passed as an argument of the mysql command
+-- lists all the cities of California in the database hbtn_0d_usa
+-- states table contains only one record where name = California
+-- Results must be sorted in ascending order by cities.id
+-- The database name will be passed as an argument of the mysql command
 
 SELECT id, name FROM cities
 WHERE state_id = (
