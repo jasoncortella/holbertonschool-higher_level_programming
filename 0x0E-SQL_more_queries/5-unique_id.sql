df0d67a3af396afc56e9639beee466eceb0a1422
Jason Cortella <419@holbertonschool.com>
2019-07-16 20:45:27 +0000

    Initial commit p5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,10 @@
+-- creates the table unique_id on your MySQL server.
+   -- unique_id description:
+      -- id INT - default value 1, must be unique
+      -- name VARCHAR(256)
+   -- The database name will be passed as an argument of the mysql command
+   -- If the table unique_id already exists, script should not fail
+
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1,
+       UNIQUE (ID),
+       name VARCHAR(256));
