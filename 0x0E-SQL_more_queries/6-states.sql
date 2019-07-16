f3db82cf0a969423187d227dad8e98810936a0a9
Jason Cortella <419@holbertonschool.com>
2019-07-16 20:57:41 +0000

    Add NOT NULL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -9,4 +9,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
        (id INT UNIQUE NOT NULL AUTO_INCREMENT,
-       name VARCHAR(256));
+       name VARCHAR(256) NOT NULL);
