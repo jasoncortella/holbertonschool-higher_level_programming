8e26723b37ea3e0cdd4bf7313cea5a0634ef858a
Jason Cortella <419@holbertonschool.com>
2019-07-16 23:20:23 +0000

    Initial commit p13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- lists all genres contained in hbtn_0d_ and the number of shows linked to each
+   -- Each record should display:
+      -- <TV Show genre> - <Number of shows linked to this genre>
+   -- Results must be sorted in descending order by the number of shows linkeda
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT name AS genre, COUNT(*) AS number_shows FROM tv_genres
+JOIN tv_show_genres ON genre_id=tv_genres.id
+GROUP BY genre_id
+ORDER BY number_shows DESC;
