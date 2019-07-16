fb2a5851b9e599223460dc1c4044352de79bf6d3
Jason Cortella <419@holbertonschool.com>
2019-07-16 22:13:34 +0000

    Initial commit p9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists all cities contained in the database hbtn_0d_usa
+   -- Each record should display: cities.id - cities.name - states.name
+   -- Results must be sorted in ascending order by cities.id
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT cities.id, cities.name, states.name FROM cities
+LEFT JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
