999c2413515372e20de83bc70743f58633865379
Jason Cortella <419@holbertonschool.com>
2019-07-16 23:20:42 +0000

    Remove table redundancy

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,7 +4,7 @@
    -- and tv_show_genres.genre_id
    -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY title, tv_show_genres.genre_id;
