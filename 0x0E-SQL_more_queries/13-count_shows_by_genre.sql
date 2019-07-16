0896f250fa053b4098b20b083626a5c8f000da93
Jason Cortella <419@holbertonschool.com>
2019-07-16 23:24:13 +0000

    Call correct table

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,5 +6,5 @@
 
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
-GROUP BY genre_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;
