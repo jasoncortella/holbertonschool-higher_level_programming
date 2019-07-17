61dff2dc54f087880f99e0f7890c7244d21e3406
Jason Cortella <419@holbertonschool.com>
2019-07-17 00:25:17 +0000

    Remove table redundancy

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
    -- Results must be sorted in descending order by the number of shows linkeda
    -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,8 +4,8 @@
    -- Results must be sorted in ascending order by the genre name
    -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ORDER BY name;
