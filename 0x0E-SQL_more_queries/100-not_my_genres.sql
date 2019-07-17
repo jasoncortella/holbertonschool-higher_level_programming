06d11403ce79ec56db2025d5a41e66c41d96af54
Jason Cortella <419@holbertonschool.com>
2019-07-17 04:07:16 +0000

    Add redundant tablename to improve clarity

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -10,4 +10,4 @@ WHERE tv_genres.id NOT IN (
       JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
       JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
       WHERE tv_shows.title = "DEXTER" )
-ORDER BY name;
+ORDER BY tv_genres.name;
