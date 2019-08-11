89cfa0cb5afedd21ae34445bf7b830cefcd81d6a
Jason Cortella <419@holbertonschool.com>
2019-08-11 03:15:04 +0000

    Switch LIKE to =

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+from the database hbtn_0e_0_usa, safe from injection
 """
 
 import MySQLdb
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                    ORDER BY states.id ASC".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                    ORDER BY states.id ASC", (argv[4],))
     rows = cursor.fetchall()
     for state in rows:
         print(state)
