624ce23a71ff41fba4a588c73f405d8c025de293
Jason Cortella <419@holbertonschool.com>
2019-08-20 20:54:15 +0000

    Change -syubject- to -subject-

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in URL, sends POST request to passed URL, displays body
-curl -sX POST -d "email=hr@holbertonschool.com&sybject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
