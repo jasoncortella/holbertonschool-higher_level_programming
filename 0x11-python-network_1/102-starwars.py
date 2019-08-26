6896766ff22dc1b18b6941a850e29344fdb42a79
Jason Cortella <419@holbertonschool.com>
2019-08-26 23:28:08 +0000

    Initial commit p13

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -11,10 +11,19 @@ if __name__ == "__main__":
     print("Number of results: {}".format(x.get("count")))
     for items in x.get("results"):
         print(items.get("name"))
+        films = items.get("films")
+        for film in films:
+            results = requests.get(film)
+            x = results.json()
+            print("\t{}".format(x.get("title")))
     next_page = x.get('next')
     while next_page:
         results = requests.get(next_page)
         x = results.json()
         for item in x.get('results'):
             print(item.get('name'))
+        for film in films:
+            results = requests.get(film)
+            x = results.json()
+            print("\t{}".format(x.get("title")))
         next_page = x.get('next')
