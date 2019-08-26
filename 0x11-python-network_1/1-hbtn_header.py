841abae2f1ad78ddcebf5a844c1a6991aa190401
Jason Cortella <419@holbertonschool.com>
2019-08-26 18:49:29 +0000

    Initial commit p0, p1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""takes in URL, sends request, displays value of X-Request-Id variable"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+         print(response.headers.get('X-Request-Id'))
