9fb6db93ecb8f46b12278fa6a9198325b7befbe2
Jason Cortella <419@holbertonschool.com>
2019-08-26 22:25:41 +0000

    Initial commit p10 - does not match output but trying checker

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""displays GitHub ID based on credentials using GitHub API"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    x = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get(url, auth=x)
+    print(r.json().get("id")
