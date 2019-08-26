c688ca0f49f17a81aa01695e165c9fd17f200933
Jason Cortella <419@holbertonschool.com>
2019-08-26 23:16:41 +0000

    Initial commit p13

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in a string and sends a search request to the Star Wars API"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people"
+    values = {"search": sys.argv[1]}
+    results = requests.get(url, params=values)
+    x = results.json()
+    print("Number of results: {}".format(x.get("count")))
+    for items in x.get("results"):
+        print(items.get("name"))
+    next_page = x.get('next')
+    while next_page:
+        results = requests.get(next_page)
+        x = results.json()
+        for item in x.get('results'):
+            print(item.get('name'))
+        next_page = x.get('next')
