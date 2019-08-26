a77dc70efed61d4d71a68863647090af2bd91d8d
Jason Cortella <419@holbertonschool.com>
2019-08-26 19:54:32 +0000

    Format compliant with pep8 standard

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email' : sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
