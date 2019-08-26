e4f0f4e37713fda545c9199a1ca108a31e5e15b2
Jason Cortella <419@holbertonschool.com>
2019-08-26 18:50:57 +0000

    Format compliant with pep8 standard

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,4 +5,4 @@ import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-         print(response.headers.get('X-Request-Id'))
+        print(response.headers.get('X-Request-Id'))
