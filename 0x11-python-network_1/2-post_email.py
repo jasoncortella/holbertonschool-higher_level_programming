c2532f8e767fe98bdea8e7fc48533e7e3a2c7bd4
Jason Cortella <419@holbertonschool.com>
2019-08-26 19:46:42 +0000

    Initial commit p2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes in URL and email, sends POST request to email, displays response"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read())
