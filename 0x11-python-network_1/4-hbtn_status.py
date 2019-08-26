3da3b5b75ac310e70af9822634def150aa6f36c1
Jason Cortella <419@holbertonschool.com>
2019-08-26 20:08:48 +0000

    Initial commit p4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
