bd4ef56b77b365a0962394ae554b393080287a34
Jason Cortella <419@holbertonschool.com>
2019-09-17 23:57:28 +0000

    Initial commit p10-13

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('0');
+} else {
+  args.sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
