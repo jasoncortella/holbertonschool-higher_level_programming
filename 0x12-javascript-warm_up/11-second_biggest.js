208ec4dd418e5eaf9ff88fc59149c1d6bf789c80
Jason Cortella <419@holbertonschool.com>
2019-09-18 04:28:57 +0000

    Account for 1 arg

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,8 @@
 const args = process.argv.slice(2);
 if (args[0] === undefined) {
   console.log('0');
+} else if (args[1] === undefined) {
+  console.log(args[0]);
 } else {
   args.sort((a, b) => a - b);
   console.log(args[args.length - 2]);
